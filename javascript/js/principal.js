e30ffe7737bc233bc8df19e114f18d820edcb557
Renata1812 <renata1812martins@gmail.com>
2019-09-04 12:09:10 -0400

    Busca de elementos de pai para filho, operadores logicos if, e, ou - 04/09

--- javascript/js/principal.js
+++ javascript/js/principal.js
@@ -1,2 +1,39 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdIMC = paciente.querySelector(".info-imc");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var pesoValido = true;
+var alturaValida = true;
+
+/*console.log(peso);
+console.log(altura);*/
+
+if(peso <= 0 || peso >= 1000){
+
+	console.log("Peso inválido");
+	tdIMC.textContent = "Peso inválido";
+	pesoValido = false;
+
+}
+
+if(altura <= 0 || altura >= 3){
+
+	console.log("Altura inválida");
+	tdIMC.textContent = "Altura invalida";
+	alturaValida = false;
+
+}
+
+if(pesoValido && alturaValida){
+
+	var imc = peso / (altura * altura);
+	tdIMC.textContent = imc;
+
+}
