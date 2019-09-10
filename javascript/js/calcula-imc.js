52d087cffd1c7b1a9f511842ae4d1c16945fd4b4
Renata1812 <renata1812martins@gmail.com>
2019-09-10 16:29:48 -0400

    Avanços das 2 aulas referentes a 10/09

--- javascript/js/calcula-imc.js
+++ javascript/js/calcula-imc.js
@@ -38,9 +38,17 @@ for(var i = 0; i < paciente.length; i++){
 
 	if(pesoValido && alturaValida){
 
-		var imc = peso / (altura * altura);
-		tdIMC.textContent = imc.toFixed(2);
+		var imc = calculaImc(peso, altura);
+		tdIMC.textContent = imc;
 
 	}
+
+}
+
+function calculaImc(peso, altura){
+
+	var imc = 0;
+	imc = peso / (altura * altura);
+	return imc.toFixed(2);
 
 }
